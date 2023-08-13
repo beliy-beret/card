69b2c0a117e412be729a1a45f5ff7b1679e21c13
AlucarD <skoriy12990@gmail.com>
2023-08-13 16:46:34 +0300

    Create SignUp form

--- src/features/auth/signUp/SignUp.tsx
+++ src/features/auth/signUp/SignUp.tsx
@@ -9,6 +9,7 @@ import { formSchema, type FormValues } from './validation'
 
 import { Button } from 'common/ui/button'
 import { Input } from 'common/ui/input'
+import { Typography } from 'common/ui/typography'
 
 type Props = {
   onSubmit: (data: FormValues) => void
@@ -32,8 +33,20 @@ export const SignUp: FC<Props> = ({ onSubmit }) => {
   return (
     <form className={css.form} onSubmit={handleSubmit(submit)}>
       <DevTool control={control} />
+      <Typography variant="Large" as="h2" className={css.title}>
+        Sign Up
+      </Typography>
 
-      <div style={{ marginBottom: errors.password ? '16px' : '' }}>
+      <div>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <Input {...field} label={'Email'} errorMessage={errors.email?.message} fullWidth />
+          )}
+        />
+      </div>
+      <div>
         <Controller
           name="password"
           control={control}
@@ -41,13 +54,14 @@ export const SignUp: FC<Props> = ({ onSubmit }) => {
             <Input
               {...field}
               type="password"
-              label="password"
+              label="Password"
               errorMessage={errors.password?.message}
+              fullWidth
             />
           )}
         />
       </div>
-      <div style={{ marginBottom: errors.confirm ? '16px' : '' }}>
+      <div>
         <Controller
           name="confirm"
           control={control}
@@ -55,17 +69,22 @@ export const SignUp: FC<Props> = ({ onSubmit }) => {
             <Input
               {...field}
               type="password"
-              label="confirm password"
+              label="Confirm password"
               errorMessage={errors.confirm?.message}
+              fullWidth
             />
           )}
         />
       </div>
 
-      <div>
-        <Button variant="primary" type="submit">
-          Submit
+      <div className={css.btnBlock}>
+        <Button variant="primary" type="submit" fullWidth>
+          Sign Up
         </Button>
+        <Typography className={css.title}>Already have an account?</Typography>
+        <a className={css.signUpLink} href="/">
+          Sign In
+        </a>
       </div>
     </form>
   )
